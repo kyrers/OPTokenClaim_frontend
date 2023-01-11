091ec287618f3f65774c019d2752ec8111539894
kyrers <kyrers.eth@gmail.com>
2023-01-11 20:20:27 +0000

    Change favicon and remove unused files

--- optokenclaim/pages/_app.tsx
+++ optokenclaim/pages/_app.tsx
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
           <title>EthernautDAO OP Token Claim</title>
           <meta name="description" content="Simple UI for claiming OP tokens being distributed by EthernautDAO" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="/edaologo.png" />
         </Head>
         <Component {...pageProps} />
       </ThemeProvider>
