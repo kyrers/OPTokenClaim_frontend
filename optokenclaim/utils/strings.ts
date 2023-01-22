d2f02b8ae939b86871393ef05724ee4c3d9a21c2
kyrers <kyrers.eth@gmail.com>
2023-01-22 19:19:56 +0000

    Minor update to help text

--- optokenclaim/utils/strings.ts
+++ optokenclaim/utils/strings.ts
@@ -2,6 +2,6 @@ export const infoText = "How does it work? \n" +
     "   1. EXP holders can call subscribe to register their EXP balance, which will be taken into account for the reward calculation of the current epoch; \n" +
     "   2. Once an epoch has passed, subscribed EXP holders will be able to claim their previous epoch reward by clicking on \"claim\"; \n" +
     "   3. Once claimed, the contract automatically resubscribes you for the next reward distribution; \n\n" +
-    "If you subscribe and then earn more EXP, you can just resubscribe with your updated EXP balance. \n\n" +
+    "If you subscribe and then earn more EXP, you can just resubscribe with your updated EXP balance. However, subscriptions are maxed at 99 EXP. \n\n" +
     "TLDR: Subscribe if you hold EXP, wait until the current epoch ends, claim your OP. Repeat every 30 days. \n\n" +
     "Still have questions? Join our Discord!";
